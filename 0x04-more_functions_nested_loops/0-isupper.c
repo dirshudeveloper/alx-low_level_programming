87a8c66e51d8c8d6bea9541a770ad79ff4b6d44e
dirshudeveloper <87856343+dirshudeveloper@users.noreply.github.com>
2022-03-22 13:25:04 +0300

    eddited

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,13 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _isupper - checks if input is an uppercase letter in ASCII
- * @c: integer to check
- * Return: 1 if true, 0 if false
+ * _isupper - check for upper case letter
+ * @c : character to check the case
+ * Return:0 or 1
  */
+
 int _isupper(int c)
 {
 	return (c >= 65 && c <= 90);
+
 }
